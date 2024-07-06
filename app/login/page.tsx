57bd960e41dc7b7c09029cf4937831ec90700aef
James Chen <dev@jamesrchen.com>
2024-07-06 15:14:05 +0800

    feat: Clearer login

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -23,7 +23,15 @@ export default function Page() {
 				<Error />
 			</Suspense>
 
-			<div className="p-10 w-full flex flex-row justify-center">
+			<h1 className="text-center text-2xl font-bold">
+				Bookance
+			</h1>
+			<h3 className="text-center text-lg font-medium">
+				Ask questions about literary texts. Quotes provided.
+			</h3>
+
+			<p className="mt-5 mb-2 text-center text-base">Sign in with your school email:</p>
+			<div className="w-full flex flex-row justify-center">
 				<motion.div
 					className="text-center border-black border-2 rounded-lg p-2 w-fit"
 					initial={{ scale: 1 }}
