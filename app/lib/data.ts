ed52509020a397eaeb3571ba271fcba934c893d9
James Chen <dev@jamesrchen.com>
2024-07-16 00:11:09 +0800

    refactor: remove useless comments

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -3,24 +3,10 @@ import { Answer, AnswerWithUser, AnswerWithUserAndBookmarked, Comment, CommentWi
 import { unstable_noStore as noStore } from 'next/cache';
 import { validateRequest } from '@/app/lib/auth';
 
-// user_id   text    not null
-// constraint bookmarks_users_id_fk
-//     references users
-//     on update cascade on delete cascade,
-// answer_id integer not null
-// constraint bookmarks_answers_id_fk
-//     references answers
-//     on update cascade on delete cascade,
-// constraint bookmarks_pk
-// primary key (user_id, answer_id)
-
 export async function fetchAnswers(userID?: string, bookmarked?: boolean) {
   noStore();
   let currentUserID = await validateRequest();
   try {
-    // const data = await sql<Answer>`SELECT * FROM answers ORDER BY id DESC`;
-    // join using foreign key user_id
-    // const userID = await validateRequest();
     let data: QueryResult<AnswerWithUserAndBookmarked>;
     if (userID) {
       if (bookmarked) {
@@ -37,7 +23,6 @@ export async function fetchAnswers(userID?: string, bookmarked?: boolean) {
           JOIN bookmarks ON answers.id = bookmarks.answer_id AND bookmarks.user_id = ${currentUserID}
           WHERE answers.user_id = ${userID} 
           ORDER BY answers.id DESC LIMIT 50`;
-        // data = await sql<AnswerWithUserAndBookmarked>`SELECT answers.*, users.name, users.picture, bookmarks.user_id IS NOT NULL as bookmarked FROM answers WHERE  JOIN users ON answers.user_id = users.id LEFT JOIN bookmarks ON answers.id = bookmarks.answer_id AND bookmarks.user_id = ${userID} ORDER BY answers.id DESC LIMIT 50`;
       }
     } else {
       data = await sql<AnswerWithUserAndBookmarked>`
@@ -46,7 +31,6 @@ export async function fetchAnswers(userID?: string, bookmarked?: boolean) {
         JOIN bookmarks ON answers.id = bookmarks.answer_id AND bookmarks.user_id = ${currentUserID}
         ORDER BY answers.id DESC LIMIT 50`;
     }
-    // console.log(data.rows)
     return data.rows
   } catch (error) {
     console.error('Database Error:', error);
@@ -79,14 +63,12 @@ export async function fetchComment(answerID: number) {
 export async function intextSearch(corpus: CorpusName, search: string) {
   // fuzzy search from corpora table column content
   try {
-    // console.log(`%${search}%`)
     let cleanSearch = search.trim()
     // If longer than 15 chars, remove leading and trailing 4 chars
     if (cleanSearch.length > 15) {
       cleanSearch = cleanSearch.slice(4, -4)
     }
     const data = await sql<CorporaSearch>`SELECT * FROM corpora WHERE id = ${corpus} AND content ILIKE ${`%${search}%`} LIMIT 3`;
-    // console.log(data)
     return data.rows;
   } catch (error) {
     console.error('Database Error:', error);
