da0fe91435e15f37aa90c05c5ac3f31ea7d98a6d
James Chen <dev@jamesrchen.com>
2024-07-16 06:02:51 +0800

    feat: add HMT and HoBA

--- config.ts
+++ config.ts
@@ -21,5 +21,63 @@ export const corpora = {
       "file-cwgOVWf9ifJgTKG78a842JeZ": "DatM Act 3 Scene 2",
       "file-5kLTitG822G7HQkAYXQYmkf3": "DatM Afterword"
     }
+  },
+  "The House of Bernarda Alba": {
+    "files": {
+      "file-U3XvKUAPY7GzX9YsVxkhods1": "HoBA Act 1",
+      "file-T5sakzlqh2gY8ZbE8zer3aYf": "HoBA Act 2",
+      "file-NB1g7f6ltUzc33qdMUOW09pL": "HoBA Act 3",
+    }
+  },
+  "The Handmaid's Tale": {
+    "files": {
+      "file-ffx4crwkPOxyvm4ylD6wKBCz": "HMT Chapter 1",
+      "file-1AJ7vU5T28dSdl3Gj4IhqrZy": "HMT Chapter 2",
+      "file-Z5Emu4z4Efk42l3mXTxqchvO": "HMT Chapter 3",
+      "file-AmLh6QMPDw62BYg5reyx4xFu": "HMT Chapter 4",
+      "file-ZU7N83Mi7PuEFhoSbOtVREwK": "HMT Chapter 5",
+      "file-6D0ac7RBgVxrQvQvBmiShQ58": "HMT Chapter 6",
+      "file-3fZ7BANhsI2j8QeGhY9cN82q": "HMT Chapter 7",
+      "file-3B9iBPaVol2G2tQt1Zd4RP0s": "HMT Chapter 8",
+      "file-Zy2ZdsDwdGrUQeZvGPrrk29b": "HMT Chapter 9",
+      "file-uzsICSAJR7ZDQO3XbDUSiQPb": "HMT Chapter 10",
+      "file-B5WcnqRZ3KBnmTOyVC1jJAZD": "HMT Chapter 11",
+      "file-oBRfMbwrDVjKD9c7BWCpziTJ": "HMT Chapter 12",
+      "file-KTXnUNlC642IzGdNjheA8IV7": "HMT Chapter 13",
+      "file-3sSilnPmUSzCfm85UYlp8lGQ": "HMT Chapter 14",
+      "file-argbjRBbCXA5eRpMy3l7viIh": "HMT Chapter 15",
+      "file-NP8AI4ZKXHYoiWbOVBk4s69n": "HMT Chapter 16",
+      "file-hQ1Ebm8Xg1X4C6dEV3e0Rff9": "HMT Chapter 17",
+      "file-CglrkDfOuOiSxzNHHmN65Wxs": "HMT Chapter 18",
+      "file-hVJrE0YiBEWJ2npOJBDxWvWC": "HMT Chapter 19",
+      "file-lJKzed8Vt3mtFofxySILSzcz": "HMT Chapter 20",
+      "file-apx118PHR4emjQTXDfGLukjh": "HMT Chapter 21",
+      "file-kHtbm1k1CPzsfvgzurRyVVA8": "HMT Chapter 22",
+      "file-5HPPeu4sQ3aWWJkcRZEQo6s8": "HMT Chapter 23",
+      "file-HegzMcjVL8DND0ae94vgmeqZ": "HMT Chapter 24",
+      "file-kElh2DXXY8I2wS4FDJZarvc7": "HMT Chapter 25",
+      "file-9RixK0OwWIKJ6jDu2ccQxOfy": "HMT Chapter 26",
+      "file-uTG3JWyuRJF6VqCG3tatdZfx": "HMT Chapter 27",
+      "file-9pyuglgs0fQJ9RMgMvvtwukV": "HMT Chapter 28",
+      "file-XHJDoWjRJf8q5ambZgZsMOa1": "HMT Chapter 29",
+      "file-1W7S5ExnbgQVA5H9KrcykpRx": "HMT Chapter 30",
+      "file-u09bwy0Jc10v8TN0ZWf66L02": "HMT Chapter 31",
+      "file-RZa4U8qc8Hv4ypDgS9rB4ZHp": "HMT Chapter 32",
+      "file-HMr8HcexhSxZNXZsGtRjEsgD": "HMT Chapter 33",
+      "file-hbn3iQYwcK5Bri1N7IEzk6p6": "HMT Chapter 34",
+      "file-zcXLhG0n7sU0h2hR0mhSGn56": "HMT Chapter 35",
+      "file-ZofNsYyo66WcXLgdGCTJYGRM": "HMT Chapter 36",
+      "file-WPYH3cApeIoPNq1enbjby8qe": "HMT Chapter 37",
+      "file-RtHWmmBKkHNu3Azhq4V4gEau": "HMT Chapter 38",
+      "file-bXgaKwq2AfLWhNP9RhBTvAzn": "HMT Chapter 39",
+      "file-UeoTx69jSp4Zj86VgwHrqijd": "HMT Chapter 40",
+      "file-aQrxYoWHMOj3quBo94WEueSE": "HMT Chapter 41",
+      "file-aAEbDA4sSvuWc8T4MVWolar9": "HMT Chapter 42",
+      "file-fRHvcgDlN8bFJXaDo6hKf396": "HMT Chapter 43",
+      "file-77NIFXJyQfbLwmpwpfOrzi0R": "HMT Chapter 44",
+      "file-f4H1kJLqvVTgjs8TqgSisnrM": "HMT Chapter 45",
+      "file-3yGA6gdS2BzGZgQ68A3iMKtv": "HMT Chapter 46",
+      "file-kXe0AwJ40MKlRW9IeBXjZSy2": "HMT Historical Notes"
+    }
   }
 } as const;
